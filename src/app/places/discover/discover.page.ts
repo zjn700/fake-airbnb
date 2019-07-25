d0fa6bedfa19d7416b3928889711fe63d0b10a0a
zjn700 <zjn700-01@yahoo.com>
2019-07-25 09:35:14 +0800

    dummy content for discover page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+  loadedPlaces: Place[];
 
-  constructor() { }
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.loadedPlaces = this.placesService.places;
   }
 
 }
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-discover',
+//   templateUrl: './discover.page.html',
+//   styleUrls: ['./discover.page.scss'],
+// })
+// export class DiscoverPage implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
